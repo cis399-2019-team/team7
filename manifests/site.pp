3dc9ba7b8acfc28bf40232cb518523baeb481d86
Derek <dswank@uoregon.edu>
2019-07-14 12:43:45 +0000

    Added cron job to automatically pull configs

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,12 @@
     node ip-10-0-0-164 {
+
+    	cron { "puppet update":
+	    command => "cd /etc/puppet && git pull -q origin master",
+	    user    => root,
+	    minute  => "*/5",
+	}
+
+
     }
 
     node ip-10-0-0.117 {
