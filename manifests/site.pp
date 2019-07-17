fda9dc12c27954aa950513e0fc3a1eeb7e31903c
Derek <dswank@uoregon.edu>
2019-07-17 04:40:22 +0000

    added a notice

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,7 @@
     node ip-10-0-0-164.us-east-2.compute.internal {
 
+    	 notice("look at me go")
+
     	cron { "puppet update":
 	    command => "cd /etc/puppet && git pull -q origin master && touch /etc/puppet/wowzersitworked",
 	    user    => root,
