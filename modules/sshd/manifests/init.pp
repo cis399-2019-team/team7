0f4a79f37b1743476dfc11514dd06579c4943704
Derek <dswank@uoregon.edu>
2019-07-17 06:33:43 +0000

    added ssh key type

--- modules/sshd/manifests/init.pp
+++ modules/sshd/manifests/init.pp
@@ -26,6 +26,7 @@ class sshd {
       }
 
 ssh_authorized_key { "fillerbit-key-pair-oregon":
+type => 'ssh-rsa',
 user => "ubuntu",
 key  => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCNvf1woMloNM3hiYnaa9HgU/jC+t+PCvmaTIbHaxinNJtq4B7fQV4ZXayyCMEwbnhwOMvXqIqyH4iDb0GbBhMv68nkXugBpUdF7EkYKUWD85ERA49UDYOgYYqz4Tqhi9L91ninS5NvGCsd4iOOcJ+fwkHzsjT2SkHQoY9NVGiwHQfZo1KyKmypN6sC1gnPHdsMT4sXtTTpUBImPeEKr8tS6SVwDsk4jYiNGR0fZJjZXQVxw4t5WSlzEP+9o+U4Nvbsi4oIa87RjothEEo/+NiO0O7wIk3TwWYV/kMP+aD1bjk5Zb8cgGXa1bSQcOck9TJBJNmmq+w4c1XfPDD18AEn",
 }
